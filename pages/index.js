6c48481a7c8964ed7c6e725b531ef5db02e01c78
Tiffany Le-Nguyen <tiffany@netlify.com>
2021-11-12 14:36:50 -0500

    chore: try server side props

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { useRouter } from 'next/router';
 
-export default function Home() {
-  const { locale } = useRouter();
-
+export default function Home({locale}) {
   return (
     <div className="container">
       <Head>
@@ -24,4 +21,20 @@ export default function Home() {
       <Footer />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const acceptLanguageHeader = context?.req?.headers['accept-language']
+  const nextLocaleCookie = context.req.cookies?.NEXT_LOCALE
+  const locale = nextLocaleCookie 
+  ? nextLocaleCookie
+  : acceptLanguageHeader 
+    ? acceptLanguageHeader
+    : context?.req?.locale || "";
+
+  return {
+    props: {
+      locale
+    }, // will be passed to the page component as props
+  }
 }

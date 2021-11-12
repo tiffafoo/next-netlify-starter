115251bdf9ffb8921c40b3d347562c51ca085645
Tiffany Le-Nguyen <tiffany@netlify.com>
2021-11-12 10:32:32 -0500

    add i18n

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es', 'fr']
+  },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Accept-Language',
+            value: 'fr;q=0.9',
+          }
+        ],
+      },
+    ]
+  }
+}
